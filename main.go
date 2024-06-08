3c6fbec5381301c87a6915d7ca86d0413a9e1e4f
Nikolaos Istatiadis <nist@ganintegrity.com>
2024-06-08 19:47:45 +0200

    Chi v5 for http routing. Get request.

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	router := chi.NewRouter()
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -21,5 +27,5 @@ func main() {
 
 // Handle HTTP routing.
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-
+	w.Write([]byte("Hello, World!"))
 }
