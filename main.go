1133fc0b0cfbefc0687344f2a303ea9a5ed6b472
Nikolaos Istatiadis <nist@ganintegrity.com>
2024-06-08 20:09:09 +0200

    Make an API.

--- main.go
+++ main.go
@@ -3,35 +3,17 @@ package main
 // Build a webserver
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/s233338/go-microservices/application"
 )
 
 func main() {
-	// Create a new router
-	router := chi.NewRouter()
+	app := application.New()
 
-	// Log all requests to the console
-	router.Use(middleware.Logger)
-
-	// Handle requests to the /hello path
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.Background())
 	if err != nil {
-		fmt.Println("Error starting server: ", err)
+		fmt.Printf("failed to start server: %v\n", err)
 	}
-}
-
-// Handle HTTP routing.
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
 }
