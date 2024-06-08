b55043f839b0945e1d70bf4f8b71eb5151d423e5
Nikolaos Istatiadis <nist@ganintegrity.com>
2024-06-08 19:19:29 +0200

    Start handle http routing.

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
+// Build a webserver
+
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
+}
+
+// Handle HTTP routing.
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+
 }
